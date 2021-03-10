e2042fc473bc40ef49682187e2e78ec09bc34b12
Mohamed Fathy <mohammad.soliman44@gmail.com>
2021-03-11 00:36:31 +0200

    migrating to sqlite DB

--- django_project/settings.py
+++ django_project/settings.py
@@ -81,12 +81,8 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mysite_db',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'USER': 'root',
-        'PASSWORD': 'mbd@1110',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
